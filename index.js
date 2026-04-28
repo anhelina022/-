9a6182277df98f07149acab83de73dbc7895ad22
Ангеліна Бебешко <angelinabebesko05@gmail.com>
2026-04-28 14:12:32 +0100

    add commit

--- index.js
+++ index.js
@@ -8,16 +8,24 @@ btn.addEventListener("click", async () => {
   let url = `https://pixabay.com/api/?key=55452633-c04e299450091da3b967905d2&q=yellow+flowers&image_type=photo&per_page=${perPage}&page=${page}`;
   try {
     const response = await fetch(url);
-    page += 1;
+    const data = await response.json();
 
-    if (page > 1) {
-      btn.textContent = "Fetch more posts";
-    }
+    data.hits.forEach((photo) => {
+      let li = document.createElement("li");
+      let img = document.createElement("img");
+
+      img.src = photo.webformatURL;
+      img.alt = photo.tags;
+      img.width = 200;
+
+      li.appendChild(img)
+      ul.appendChild(li)
+    });
+
+    page += 1;
+    btn.textContent = "Fetch more posts";
+
   } catch (error) {
     console.log(error);
   }
 });
-
-function postPhoto(ul) {
-    `<li><img src="https://posterjack.ca/cdn/shop/articles/landscape_photography_tips_featured_image.jpg?v=1563408049&width=1500" with="150px"heith="200px"></li>`;
-  }
